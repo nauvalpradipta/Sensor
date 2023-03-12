ab3756edf10b04f139424ee161609a43c38cccd5
nauvalpradipta <nauvalpradipta2@gmail.com>
2023-03-12 08:55:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/readsensordata/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.readsensordata;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensorLight;
+    private Sensor mSensorProximity;
+
+    private TextView mTextSensorLight;
+    private TextView mTextSensorProximity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        StringBuilder sensorText = new StringBuilder();
+        for (Sensor currentSensor : sensorList) {
+            sensorText.append(currentSensor.getName())
+                    .append(System.getProperty("line.separator"));
+        }
+        TextView sensorTextView = findViewById(R.id.sensor_list);
+        sensorTextView.setText(sensorText);
+
+        mTextSensorLight = findViewById(R.id.label_light);
+        mTextSensorProximity = findViewById(R.id.label_proximity);
+
+        mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        String sensor_error = "No sensor";
+
+        if (mSensorLight == null) {
+            mTextSensorLight.setText(sensor_error);
+        }
+        if (mSensorProximity == null) {
+            mTextSensorProximity.setText(sensor_error);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mSensorProximity != null) {
+            mSensorManager.registerListener(this, mSensorLight,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorLight != null) {
+            mSensorManager.registerListener(this, mSensorProximity,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        int sensorType = sensorEvent.sensor.getType();
+        float currentValue = sensorEvent.values[0];
+
+        switch (sensorType) {
+            case Sensor.TYPE_LIGHT:
+                mTextSensorLight.setText(
+                        String.format("Light sensor : %1$.2f", currentValue));
+                changeBackgroundColor(currentValue);
+                break;
+            case Sensor.TYPE_PROXIMITY:
+                mTextSensorProximity.setText(
+                        String.format("Proximity sensor : %1$.2f", currentValue));
+                break;
+            default:
+                //
+        }
+    }
+
+    private void changeBackgroundColor(float currentValue) {
+        ConstraintLayout layout = findViewById(R.id.layout_constraint);
+        if(currentValue <= 40000 && currentValue >= 20000) layout.setBackgroundColor(Color.RED);
+        else if(currentValue < 20000 && currentValue >= 10) layout.setBackgroundColor(Color.BLUE);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
